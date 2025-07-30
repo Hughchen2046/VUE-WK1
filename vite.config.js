d18a57046ae0e5dbdc2f5c88e22f4827eea27129
Hugh <hughchen2046@gmail.com>
2025-07-31 01:57:01 +0800

    Build deploy

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,9 @@ import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
+
 export default defineConfig({
+  base: '/VUE-WK1/',
   plugins: [
     vue(),
     vueDevTools(),
